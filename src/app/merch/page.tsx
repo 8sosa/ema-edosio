d440294d9a3d4f78795eb1898c866ab2903bcb85
8sosa <emmanuel.amos@stu.cu.edu.ng>
2025-03-29 21:45:13 +0100

    updayte

--- src/app/merch/page.tsx
+++ src/app/merch/page.tsx
@@ -4,32 +4,6 @@ import './page.css'
 export default function HomePage() {
   return (
     <>
-      <section className="relative w-full h-[90vh] bg-black overflow-hidden">
-        {/* Background Image */}
-        <div className="absolute inset-0">
-          <Image
-            src="/images/ph.png"
-            alt="Hero Image"
-            fill
-            className="object-cover object-center"
-          />
-        </div>
-        {/* Overlay */}
-        <div className="absolute inset-0 overlay" />
-        {/* Content */}
-        <div className="relative z-10 flex flex-col items-start justify-center h-full max-w-5xl mx-auto px-6">
-          <h1 className="text-3xl md:text-5xl font-bold mb-2 title">
-            Jackets are like wigs.
-          </h1>
-          <p className="text-lg md:text-xl text-gray-700 max-w-xl mb-4 body">
-            Urban style redefined. Stay comfortable and trendy with our latest collection.
-          </p>
-          <button className=" text bg-black text-white px-6 py-3 rounded-full hover:bg-gray-800 transition">
-            Discover Now
-          </button>
-        </div>
-      </section>
-
       {/* NEW ARRIVALS SECTION */}
       <section className="py-12 px-6 max-w-8xl">
         <h2 className="title text-2xl md:text-3xl font-bold mb-6 text-center">
